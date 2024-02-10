2e9ef52ffd297066e659b04a917d4b438d7f9aa0
Galileo <eskimasal@outlook.com>
2024-02-11 01:28:49 +0300

    Delete monolithic/express/freelancerPortfolio directory

--- monolithic/express/freelancerPortfolio/routes/pageRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-
-const pageController = require("../controllers/pageController");
-
-Router.route("/").get(pageController.getHomePage);
-Router.route("/about").get(pageController.getAboutPage);
-
-module.exports = Router;
